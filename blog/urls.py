823e1d68edf4c960ab5d2b264480b207e1f0031e
Trivikram <trivikram4567@gmail.com>
2024-08-31 23:22:48 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.starting_page, name="stating-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail")
+]
